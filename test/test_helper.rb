67abdd195fa64a854c789ac469e9e4eac7fc7457
Tim Cheadle <tim@rationalmeans.com>
2015-08-24 14:15:36 -0400

    Include capybara DSL in integration tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,6 +12,10 @@ require 'capybara/rails'
 require 'capybara/poltergeist'
 Capybara.javascript_driver = :poltergeist
 
+class ActionDispatch::IntegrationTest
+    include Capybara::DSL
+end
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
