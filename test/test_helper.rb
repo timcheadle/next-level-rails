f9f3d94781a65db2b5c529f5d28af7a8db964521
Tim Cheadle <tim@rationalmeans.com>
2015-08-24 14:10:55 -0400

    Added test_helper and Guard configuration

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,16 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'mocha/mini_test'
+
+# Improved test output
+require 'minitest/reporters'
+Minitest::Reporters.use!(Minitest::Reporters::ProgressReporter.new)
+
+# Capybara and poltergeist for feature testing
+require 'capybara/rails'
+require 'capybara/poltergeist'
+Capybara.javascript_driver = :poltergeist
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
