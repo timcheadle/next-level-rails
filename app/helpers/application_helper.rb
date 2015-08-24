ba187137078b047ddc35cf67b5c2c741177d51d8
Tim Cheadle <tim@rationalmeans.com>
2015-08-24 16:16:15 -0400

    Added a few view helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+  def is_active?(action)
+    'active' if params[:controller] == 'welcome' && params[:action] == action
+  end
+
+  def markdown(&block)
+    content = capture(&block)
+    renderer = Redcarpet::Render::HTML
+    Redcarpet::Markdown.new(renderer).render(content).html_safe
+  end
+
+  def flash_class(level)
+    case level
+      when 'notice' then 'alert-info'
+      when 'success' then 'alert-success'
+      when 'error' then 'alert-danger'
+      when 'alert' then 'alert-danger'
+    end
+  end
 end
